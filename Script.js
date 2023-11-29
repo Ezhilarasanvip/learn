7a2c0197cbe542685df93f16d23e2f00cfe5d9c1
Ezhilarasan M <vipezhilarasan@gmail.com>
2023-11-29 18:01:21 +0530

    undefined

--- Script.js
+++ Script.js
@@ -7,6 +7,31 @@
 
 // console.log(person.name);
 
-let selectedColors = ['Red', 'blue', 'green'];
-selectedColors[3] = 'yellow';
-console.log(selectedColors.length)
+// let selectedColors = ['Red', 'blue', 'green'];
+// selectedColors[3] = 'yellow';
+// console.log(selectedColors.length)
+
+// function great(name, lastName){
+//     console.log("Welcome "+name +lastName);
+// }
+// great("Ezhil", "arasan");
+
+// function squre(number){
+//     return number*number;
+// }
+// console.log(squre(2));
+
+// let points = 100;
+// let type = points >=100 ? 'gold' : 'silver';
+// console.log(type);
+
+// let hightIncome = false;
+// let goodScore = false;
+
+// let isElibale = hightIncome && goodScore;
+// console.log(isElibale);
+/*don't have color it will take defaultColor*/
+// let userColor = undefined;
+// let defaultColor = 'blue';
+// let currentColor = userColor || defaultColor;
+// console.log(currentColor);
